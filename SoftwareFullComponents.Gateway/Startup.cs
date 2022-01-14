bb7c0920398f12168de060ded4f7f2d91491d262
Stitch <st.van.eekelen@gmail.com>
2022-01-14 22:02:36 +0100

    Doesn't work

--- SoftwareFullComponents.Gateway/Startup.cs
+++ SoftwareFullComponents.Gateway/Startup.cs
@@ -53,7 +53,7 @@ namespace WebGateway
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment() || env.IsProduction())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
