fd608a5cd5b6afbbd0591004c75c4c6d050d888a
Stitch <st.van.eekelen@gmail.com>
2022-01-14 20:19:09 +0100

    Test if azure auto updates

--- SoftwareFullComponents.Gateway/Startup.cs
+++ SoftwareFullComponents.Gateway/Startup.cs
@@ -66,7 +66,7 @@ namespace WebGateway
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Docker works?");
                 });
             });
 
